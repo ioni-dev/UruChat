f4fa729038ac09bb974ac988fc264da740c81ad6
ioni <ioni_dev@protonmail.com>
2019-06-22 10:13:52 -0300

    initial

---
+++ client/main.js
@@ -0,0 +1,45 @@
+import { Template } from "meteor/templating";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./main.html";
+import { Messages } from '../lib/collections.js';
+import { Meteor } from 'meteor/meteor';
+
+// Aqui me subscribo a la publicacion del servidor
+Meteor.subscribe('messages.server', 5);
+
+
+// Aqui defino el comportamiento que permite mostrar los mensajes
+Template.chatBody.helpers({
+  messages: function() {
+    return Messages.find({});
+  }
+});
+
+
+// Controla el envio de el mensaje
+Template.chatInput.events({
+  'submit .writeMessageForm': function (event) {
+    event.preventDefault();
+    let text = event.target.writeMessageInput.value;
+    console.log(text);
+    // Aqui llamo al metodo del servidor y le paso el la variable
+    // que contine el texto que escribe la persona
+    Meteor.call('insertMessage', text, function(err, result){
+      if(err) {
+        console.log(err.reason);
+      } else {
+         console.log('sended by: ', result);
+         event.target.writeMessageInput.value = '';
+      }
+    });
+    Messages.insert({
+      text: text,
+      timestamp: Date.now()
+    });
+  }
+});
+
+// Configuracion del account
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY'
+});
---
+++ server/main.js
@@ -0,0 +1,33 @@
+import { Meteor } from 'meteor/meteor';
+import { Messages } from '../lib/collections.js';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+
+});
+Meteor.methods({
+  insertMessage: function(text) {
+    var user = Meteor.users.findOne(this.userId);
+      // Inserto el mensaje
+    console.log(user)
+    return Messages.insert({
+      userId: this.userId,
+      username: user.username,
+      text: text,
+      timestamp: Date.now()
+    });
+  }
+});
+
+Messages.remove({});
+// Retornando el mongo cursor, esto esta disponible para el cliente
+// Uso function en lugar de arrow para hacer uso del this
+
+Meteor.publish('messages.server', function(limit) {
+
+  return Messages.find({}, {
+    limit: limit || 5,
+    sort: { timestamp: -1 }
+  });
+  this.ready();
+});
