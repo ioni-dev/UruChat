15a4a5fde31ff698dfd284e30964c5fbd63dfce7
ioni <ioni_dev@protonmail.com>
2019-06-24 00:09:06 -0300

    final version

--- client/main.js
+++ client/main.js
@@ -4,40 +4,41 @@ import "./main.html";
 import { Messages } from '../lib/collections.js';
 import { Meteor } from 'meteor/meteor';
 
-// Aqui me subscribo a la publicacion del servidor
-Meteor.subscribe('messages.server', 5);
+// Here is my subscription to the publish method in the server
+Meteor.subscribe('messages.server');
 
-
-// Aqui defino el comportamiento que permite mostrar los mensajes
-// en el helper
+// This is the method that deliver the collection content to the html
+// in that way i can iterate and render each text and author
 Template.chatBody.helpers({
   messages: function() {
-    return Messages.find({},{
-    });
-  }
-});
+    return Messages.find().fetch();
+  }});
 
-
-// Controla el envio de el mensaje
+// This is where i capture the text value of the input
+// when i submit the form
 Template.chatInput.events({
   'submit .writeMessageForm': function (event) {
     event.preventDefault();
     let text = event.target.writeMessageInput.value;
-    // Aqui llamo al metodo del servidor y le paso el la variable
-    // que contine el texto que escribe la persona
+    // I placed the call method here because i can pass the text variable
+    // to the Meteor.method in the server in an easy way
     Meteor.call('insertMessage', text, function(err, result){
+      // I can capture the error if there's any and call this meteor methods
+      // following the meteor docs this is the best way.
       if(err) {
         throw new Meteor.Error('message not found', "Can't find any message");
       } else {
+        // With this i can clear my input box, so the text that was sended
+        // can no longer apper, it's more cleaner and not confusing to the user
        event.target.writeMessageInput.value = ' ';
-
       }
     });
 
   }
 });
 
-// Configuracion del account
+// This is the account configuration, i was consulting the Meteor docs
+// to make the user register and login with username and password
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY'
 });
--- server/main.js
+++ server/main.js
@@ -1,37 +1,46 @@
 import { Meteor } from 'meteor/meteor';
 import { Messages } from '../lib/collections.js';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+// To be honest, i don't need this method for this app, that's why it's commented
+// Meteor.startup(() => {
+//   // code to run on server at startup
+//
+// });
 
-});
+// I defined the insertMessage to insert the data to the mongo Collection
 Meteor.methods({
   insertMessage: function(text) {
+    // First the user verification, it throws a simple error
     if(!this.userId){
       alert("user not available");
     }
+    // I get the current user
     var user = Meteor.users.findOne(this.userId);
-      // Inserto el mensaje
+
     return Messages.insert({
       userId: this.userId,
       username: user.username,
       text: text,
-      timestamp: Date.now()
+      timestamp: Date.now(),
+      class: ''
     });
   }
 });
 
 Messages.remove({});
-// publish un set de records (nombre del set, callback)
-// Uso function en lugar de arrow para hacer uso del this
+// publish a set of records (set name, callback)
 
-Meteor.publish('messages.server', function(limit) {
-// para usarios loggeados
+
+Meteor.publish('messages.server', function() {
+// for logged users
  if(this.userId){
    return Messages.find({}, {
+     // the sorting solution, in this way i can show the recent message
+     // at the bottom
       sort: { timestamp: -1 }
   });
 }
-// Llama al subscrie del cliente
+// this is helpful to notify the subscribe method from the client side
+// that a new change has been made and it's completed.
   this.ready();
 });
